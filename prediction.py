9cddf3b73e3802aecbd2776c740479df854d9c03
raouftams <raouftams@gmail.com>
2022-04-24 02:41:33 +0100

    tweak regressors parameters to get the best ones

--- prediction.py
+++ prediction.py
@@ -2,7 +2,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.svm import SVR
 from sklearn.neural_network import MLPRegressor
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.preprocessing import LabelEncoder, MinMaxScaler, StandardScaler
 from datetime import datetime
 import pandas as pd
 import numpy as np
@@ -21,7 +21,7 @@ def encode_data(df):
     #    df['cet'] = df['cet'].replace([item[0]], item[1])
     
     #encode seasons values
-    seasons = {"winter": 1, "spring":2, "summer":3, "autumn":4}
+    seasons = {"winter": 0.1, "spring":0.2, "summer":0.3, "autumn":0.4}
     for season in seasons.items():
         df['season'] = df['season'].replace([season[0]], season[1])
 
@@ -32,7 +32,7 @@ def encode_data(df):
     df['day'] = df['date'].dt.day
 
     #encode holidays
-    holidays = {"normal": 1, "Ramadhan":2, "Eid al-Fitr":3, "Eid al-Adha":4, "Islamic New Year": 5, "Ashura": 6, "Prophet's Birthday": 7}
+    holidays = {"normal": 0.1, "Ramadhan":0.2, "Eid al-Fitr":0.3, "Eid al-Adha":0.4, "Islamic New Year": 0.5, "Ashura": 0.6, "Prophet's Birthday": 0.7}
     for holiday in holidays.items():
         df['holiday'] = df['holiday'].replace([holiday[0]], holiday[1])
     
@@ -40,7 +40,6 @@ def encode_data(df):
     towns = np.unique(df['code_town'].to_numpy())
     for town in towns:
         code = int(town[1:])
-        print(town, code)
         df['code_town'] = df['code_town'].replace([town], code)
     
     return df
@@ -55,7 +54,7 @@ def preprocess_data(df: pd.DataFrame):
     df.sort_values(by='date', ascending=True, inplace=True)
 
     #get data trend
-    trend = data_trend(df.copy(), 'net', model='additive', period=365).to_frame('values')
+    trend = data_trend(df.copy(), 'net', model='additive', period=30).to_frame('values')
     df['date'] = pd.to_datetime(df['date'])
     df = df.merge(trend, on='date', how='inner')
     df['values'] = df['values'].astype(int)
@@ -67,10 +66,11 @@ def preprocess_data(df: pd.DataFrame):
     df.loc[df['date'].dt.year == 2019, 'population'] = df[df['date'].dt.year == 2019]['pop2019']
     df.loc[df['date'].dt.year == 2020, 'population'] = df[df['date'].dt.year == 2020]['pop2020']
     df.loc[df['date'].dt.year == 2021, 'population'] = df[df['date'].dt.year == 2021]['pop2021']
+    df.loc[df['date'].dt.year == 2022, 'population'] = df[df['date'].dt.year == 2022]['pop2022']
     #encode data
     df = encode_data(df)
     #shift t-1 and t+1 of class data
-    for i in range(1, 10):
+    for i in range(1, 4):
         col = 't-'+str(i)
         df[col] = df['values'].shift(i)
 
@@ -82,11 +82,10 @@ def preprocess_data(df: pd.DataFrame):
     df = df.drop_duplicates(subset=['date', 'code_town'])
 
     df.drop(['latitude', 'longitude', 'code_ticket', 'code_unity', 'holiday', 'season', 'date', 'net', 'values', 'date_hijri', 'cet', 'pop2016', 'pop2017', 'pop2018', 'pop2019', 'pop2020', 'pop2021','pop2022'], axis=1, inplace=True)
-    
     #normalize data
-    #normalized = StandardScaler().fit_transform(df)
+    #normalized = MinMaxScaler().fit_transform(df)
     #return normalized
-
+    
     return df.to_numpy()
 
 def gbrt():
@@ -96,12 +95,12 @@ def gbrt():
     #clean, encode and normalize data
     prepared_data = preprocess_data(df)
     
-    y = prepared_data[:, 14]
-    X = prepared_data[:, :14]
+    y = prepared_data[:, 8]
+    X = prepared_data[:, :8]
     
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=False, random_state=0)
     
-    reg = GradientBoostingRegressor(learning_rate=0.1, n_estimators=1000)
+    reg = GradientBoostingRegressor(learning_rate=0.03, n_estimators=1000)
     reg.fit(X_train, y_train)
     print(reg.score(X_test, y_test))
     y_pred = reg.predict(X_test)
@@ -111,7 +110,7 @@ def gbrt():
     plt.plot(y_pred)
     plt.show()
     
-    #savePkl(reg, "models/gbrt_by_town.pkl")
+    savePkl(reg, "models/gbrt_by_town.pkl")
 
 def svr():
     #get all data stored in db
@@ -120,31 +119,12 @@ def svr():
     #clean, encode and normalize data
     prepared_data = preprocess_data(df)
 
-    y = prepared_data[:, 4]
-    X = prepared_data[:, :4]
+    y = prepared_data[:, 8]
+    X = prepared_data[:, :8]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=0)
 
-    reg = SVR(C=1)
-    reg.fit(X_train, y_train)
-    print(reg.score(X_test, y_test))
-    y_pred = reg.predict(X_test)
-    print(y_pred[:5])
-    print(y_test[:5])
-
-def mlpr():
-    #get all data stored in db
-    df = get_data()
-    df = df[df["code_town"] == 'C002']
-    #clean, encode and normalize data
-    prepared_data = preprocess_data(df)
-
-    y = prepared_data[:, 14]
-    X = prepared_data[:, :14]
-
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, shuffle=False, random_state=0)
-
-    reg = MLPRegressor(hidden_layer_sizes=(160,), activation="relu", solver='adam', max_iter=200)
+    reg = SVR()
     reg.fit(X_train, y_train)
     print(reg.score(X_test, y_test))
     y_pred = reg.predict(X_test)
@@ -155,24 +135,59 @@ def mlpr():
     plt.show()
 
 
+def mlpr(df: pd.DataFrame):
+
+    df = df[df["code_town"] == 'C002']
+    #clean, encode and normalize data
+    prepared_data = preprocess_data(df)
+
+    y = prepared_data[:, 8]
+    X = prepared_data[:, :8]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False, random_state=0)
+
+    reg = MLPRegressor(hidden_layer_sizes=(10,), activation="relu", solver='adam', max_iter=500)
+    reg.fit(X, y)
+    #print(reg.score(X_test, y_test))
+    #y_pred = reg.predict(X_test)
+    #print(y_pred[:5])
+    #print(y_test[:5])
+    #plt.plot(y_test)
+    #plt.plot(y_pred)
+    #plt.show()
+    savePkl(reg, "models/mlp_by_town.pkl")
+
 def main():
-    mlpr()
-    gbrt()
+    train_df = get_data()
+    mlpr(train_df)
+    old_df = get_data()
+    old_df["net"] = old_df.groupby(['code_town', 'date'])["net"].transform('sum')
+    old_df.drop_duplicates(subset=['code_town', 'date'], inplace=True)
+    old_df["date"] = pd.to_datetime(old_df["date"])
+    old_df.sort_values(by='date', ascending=True, inplace=True)
+    old_df = old_df[old_df['code_town'] == 'C002']
+    new_df = old_df[old_df["date"].dt.year == 2021]
+    new_df['date'] = new_df['date'].apply(lambda x: x.replace(year = x.year + 1))
+    new_df.drop_duplicates(subset=['date', 'code_town'], inplace=True)
+    add_hijri_holidays(new_df)
+    new_df['net'] = 0
+
+    print(old_df)
+    print(new_df)
+    model = openPkl('models/mlp_by_town.pkl')
+    for index, row in new_df.iterrows():
+        old_df = old_df.append(row, ignore_index=True)
+        array = preprocess_data(old_df)
+        size = array.shape[0]
+        last_row = [array[size-1, :8]]
+        y_pred = model.predict(last_row)
+        old_df.iloc[-1, old_df.columns.get_loc('net')] = y_pred[0]
+        
+            
+    plt.plot(array[:, 8])
+    plt.show()
     """
-    df = get_data()
-    df["net"] = df.groupby(['code_town', 'date'])["net"].transform('sum')
-    df.drop_duplicates(subset=['code_town', 'date'], inplace=True)
-    df["date"] = pd.to_datetime(df["date"])
-    df = df[df['code_town'] == 'C002']
-    df = df[df["date"].dt.year == 2021]
-    print(df)
-    df['date'] = df['date'].apply(lambda x: x.replace(year = x.year + 1))
-    df.sort_values(by='date', ascending=True, inplace=True)
-    df.drop_duplicates(subset=['date', 'code_town'], inplace=True)
-    dates = df['date'].to_numpy()
-    plt.plot(df["date"], df["net"])
-    add_hijri_holidays(df)
-    array = preprocess_data(df)
+    array = preprocess_data(row)
     X = array[:, :4]
     model = openPkl("models/gbrt_by_town.pkl")
     y_pred = model.predict(X)
